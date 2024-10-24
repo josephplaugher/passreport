f9d8d9afe1f81f98c3893c43641c6af374a8521b
josephplaugher <37582996+josephplaugher@users.noreply.github.com>
2024-10-23 20:54:31 -0700

    Update ITestResult.ts

--- src/interfaces/ITestResult.ts
+++ src/interfaces/ITestResult.ts
@@ -1,24 +1,45 @@
 import { ScoreEnum, SubjectEnum } from "../enums/Enums";
 
+interface ITestResultProperty {
+    nameof?: string;
+    value: number | SubjectEnum | ScoreEnum;
+}
+
 export interface ITestResult {
-    customerId: number;
-    studentId: number;
-    testId: number;
-    subject: SubjectEnum;
-    testLevel: number;
-    rawScore: number;
-    percentCorrect: number;
-    ritScore: number;
-    hewittPercentile: number;
-    natlPercentile: number;
-    firstGoalRank: ScoreEnum;
-    secondGoalRank: ScoreEnum;
-    thirdGoalRank: ScoreEnum;
-    fourthGoalRank: ScoreEnum;
-    fifthGoalRank: ScoreEnum;
-    sixthGoalRank: ScoreEnum;
-    seventhGoalRank: ScoreEnum;
-    responses: string;
+    // customerId: number;
+    // studentId: number;
+    // testId: number;
+    // subject: string;
+    // testLevel: number;
+    // rawScore: number;
+    // percentCorrect: number;
+    // ritScore: number;
+    // hewittPercentile: number;
+    // natlPercentile: number;
+    // firstGoalRank: number;
+    // secondGoalRank: number;
+    // thirdGoalRank: number;
+    // fourthGoalRank: number;
+    // fifthGoalRank: number;
+    // sixthGoalRank: number;
+    // seventhGoalRank: number;
+    customerId: ITestResultProperty
+    studentId: ITestResultProperty
+    testId: ITestResultProperty
+    subject: ITestResultProperty
+    testLevel: ITestResultProperty
+    rawScore: ITestResultProperty
+    percentCorrect: ITestResultProperty
+    ritScore: ITestResultProperty
+    hewittPercentile: ITestResultProperty
+    natlPercentile: ITestResultProperty
+    firstGoalRank: ITestResultProperty
+    secondGoalRank: ITestResultProperty
+    thirdGoalRank: ITestResultProperty
+    fourthGoalRank: ITestResultProperty
+    fifthGoalRank: ITestResultProperty
+    sixthGoalRank: ITestResultProperty
+    seventhGoalRank: ITestResultProperty
 }
 
 export interface IResultExplanation {
@@ -29,11 +50,11 @@ export interface IResultExplanation {
     fifthGoalScore: ScoreEnum;
     sixthGoalScore: ScoreEnum;
     seventhGoalScore: ScoreEnum;
-    firstGoalExplan: string | null;
-    secondGoalExplan: string | null;
-    thirdGoalExplan: string | null;
-    fourthGoalExplan: string | null;
-    fifthGoalExplan: string | null;
-    sixthGoalExplan: string | null;
-    seventhGoalExplan: string | null;
+    firstGoalExplan?: string;
+    secondGoalExplan?: string;
+    thirdGoalExplan?: string;
+    fourthGoalExplan?: string;
+    fifthGoalExplan?: string;
+    sixthGoalExplan?: string;
+    seventhGoalExplan?: string;
 }
